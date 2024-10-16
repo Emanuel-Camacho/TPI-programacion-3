e8a7c05c4f19034ecacfd7ac696c060c1b9fcf3f
Felipe Gil <felipeegil05@gmail.com>
2024-10-16 15:35:50 -0300

    Corrección error ejecutar

---
+++ src/Web/Controllers/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class User : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("");
+        }
+    }
+}
