293fdbcdaf568ecbd876b03b4eaee0f17af867bc
Felipe Gil <felipeegil05@gmail.com>
2024-10-30 11:59:44 -0300

    30/10

--- src/Infrastructure/Data/ApplicationContext.cs
+++ src/Infrastructure/Data/ApplicationContext.cs
@@ -9,7 +9,6 @@ namespace Infrastructure.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Admin> Admins { get; set; }
         public DbSet<Client> Clients { get; set; }
-        public DbSet<Item> Items { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
     }
---
+++ src/Infrastructure/Data/Migrations/20241030145833_addClientId.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addClientId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Users_clientId",
+                table: "Carts");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_clientId",
+                table: "Carts");
+
+            migrationBuilder.RenameColumn(
+                name: "clientId",
+                table: "Carts",
+                newName: "ClientId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CartId",
+                table: "Products",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CartId",
+                table: "Products",
+                column: "CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_ClientId",
+                table: "Carts",
+                column: "ClientId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Users_ClientId",
+                table: "Carts",
+                column: "ClientId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Carts_CartId",
+                table: "Products",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Users_ClientId",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Carts_CartId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CartId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_ClientId",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "ClientId",
+                table: "Carts",
+                newName: "clientId");
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    productId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CartId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    UnitPrice = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Items_Carts_CartId",
+                        column: x => x.CartId,
+                        principalTable: "Carts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Items_Products_productId",
+                        column: x => x.productId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_clientId",
+                table: "Carts",
+                column: "clientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_CartId",
+                table: "Items",
+                column: "CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_productId",
+                table: "Items",
+                column: "productId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Users_clientId",
+                table: "Carts",
+                column: "clientId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
